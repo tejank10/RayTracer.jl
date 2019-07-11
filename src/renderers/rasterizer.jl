2133224805cedd3f71f9eb803faa902aeb96f202
Tejan Karmali <tejank10@gmail.com>
2019-07-11 17:06:51 +0530

    rm broadcast stuff

--- src/renderers/rasterizer.jl
+++ src/renderers/rasterizer.jl
@@ -86,54 +86,42 @@ function rasterize(cam::Camera{T}, scene::Vector, camera_to_world,
 
         y = y₁:y₂
         x = x₁:x₂
+        for y_val in y₁:y₂
+            for x_val in x₁:x₂
+                pixel = Vec3([x_val+0.5f0], [y_val+0.5f0], zeros(Float32, 1))
+                w1_val = edge_function(v2_raster, v3_raster, pixel)
+                w2_val = edge_function(v3_raster, v1_raster, pixel)
+                w3_val = edge_function(v1_raster, v2_raster, pixel)
 
-        y_space = repeat(collect(y), inner = length(x))
-        x_space = repeat(collect(x), outer = length(y))
+                if w1_val >= 0 && w2_val >= 0 && w3_val >= 0
+                    w1_val = w1_val / area
+                    w2_val = w2_val / area
+                    w3_val = w3_val / area
 
-        w1_arr = Float32[]
-        w2_arr = Float32[]
-        w3_arr = Float32[]
-        depth  = Float32[]
-        x_arr  = Int[]
-        y_arr  = Int[]
+                    depth_val = 1 / (w1_val / v1_raster.z[] + w2_val / v2_raster.z[] +
+                                     w3_val / v3_raster.z[])
 
-        y_vec = y_space .+ 0.5f0
-        x_vec = x_space .+ 0.5f0
+                    if depth_val < depth_buffer[y_val*width+x_val+1]
+                        update_index!(depth_buffer, y_val*width+x_val+1, depth_val)
 
-        pixel = Vec3(x_vec, y_vec, zeros(eltype(x_vec), length(x) * length(y)))
-        w1 = edge_function_vector(v2_raster, v3_raster, pixel)
-        w2 = edge_function_vector(v3_raster, v1_raster, pixel)
-        w3 = edge_function_vector(v1_raster, v2_raster, pixel)
+                        px = (v1_camera.x[] / -v1_camera.z[]) .* w1_val .+
+                             (v2_camera.x[] / -v2_camera.z[]) .* w2_val .+
+                             (v3_camera.x[] / -v3_camera.z[]) .* w3_val
 
-        for (w1_val, w2_val, w3_val, x_val, y_val) in zip(w1, w2, w3, x_space, y_space)
-            if w1_val >= 0 && w2_val >= 0 && w3_val >= 0
-                w1_val = w1_val / area
-                w2_val = w2_val / area
-                w3_val = w3_val / area
+                        py = (v1_camera.y[] / -v1_camera.z[]) .* w1_val .+
+                             (v2_camera.y[] / -v2_camera.z[]) .* w2_val .+
+                             (v3_camera.y[] / -v3_camera.z[]) .* w3_val
 
-                depth_val = 1 / (w1_val / v1_raster.z[] + w2_val / v2_raster.z[] +
-                                 w3_val / v3_raster.z[])
+                        # Passing these gradients as 1.0f0 is incorrect
+                        pt = Zygote.hook(Δ -> Vec3([1.0f0 for _ in pt.x]),
+                                         camera2world(Vec3(px*depth_val, py*depth_val, -depth_val),
+                                                      camera_to_world))
 
-                if depth_val < depth_buffer[y_val*width+x_val+1]
-                    update_index!(depth_buffer, y_val*width+x_val+1, depth_val)
+                        col = get_color(triangle, pt, Val(:diffuse))
+                        idx = y_val .* width .+ x_val .+ 1
 
-                    px = (v1_camera.x[] / -v1_camera.z[]) .* w1_val .+
-                         (v2_camera.x[] / -v2_camera.z[]) .* w2_val .+
-                         (v3_camera.x[] / -v3_camera.z[]) .* w3_val
-
-                    py = (v1_camera.y[] / -v1_camera.z[]) .* w1_val .+
-                         (v2_camera.y[] / -v2_camera.z[]) .* w2_val .+
-                         (v3_camera.y[] / -v3_camera.z[]) .* w3_val
-
-                    # Passing these gradients as 1.0f0 is incorrect
-                    pt = Zygote.hook(Δ -> Vec3([1.0f0 for _ in pt.x]),
-                                     camera2world(Vec3(px*depth_val, py*depth_val, -depth_val),
-                                                  camera_to_world))
-
-                    col = get_color(triangle, pt, Val(:diffuse))
-                    idx = y_val .* width .+ x_val .+ 1
-
-                    frame_buffer = place_idx!(frame_buffer, col, idx)
+                        frame_buffer = place_idx!(frame_buffer, col, idx)
+                    end
                 end
             end
         end
